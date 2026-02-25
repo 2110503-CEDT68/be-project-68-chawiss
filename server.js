f51f611dc7d5a6ecef31b35b253764f73113cde0
Brightpmk <bright19699@gmail.com>
2026-02-25 14:59:44 +0700

    initialize project with assignment 7 base structure

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db.js')
+const hospitals = require('./routes/hospitals');
+const auth = require('./routes/auth');
+const cookieParser = require('cookie-parser')
+const appointments = require('./routes/appointments');
+dotenv.config({ path: './config/config.env' });
+
+connectDB();
+
+const app=express();
+app.set('query parser', 'extended');
+//Body parser
+app.use(express.json());
+app.use(cookieParser());
+app.use('/api/v1/hospitals',hospitals);
+app.use('/api/v1/auth', auth);
+app.use('/api/v1/appointments', appointments);
+
+const PORT = process.env.PORT || 5000;
+const server = app.listen(PORT, () => {
+    console.log(
+        'Server running in',
+        process.env.NODE_ENV,
+        'mode on port',
+        PORT
+    );
+});
+
+//Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error: ${err.message}`);
+
+    // Close server & exit process
+    server.close(() => process.exit(1));
+});
